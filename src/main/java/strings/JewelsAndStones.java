ddab0012c8c4008db0d9d9017655fe5a27e76543
poacher286 <bulandsahu50@gmail.com>
2022-07-31 20:59:34 +0530

    JewelsAndStones added

---
+++ src/main/java/strings/JewelsAndStones.java
@@ -0,0 +1,23 @@
+package strings;
+
+public class JewelsAndStones {
+    /**
+     * You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+     *
+     * Letters are case sensitive, so "a" is considered a different type of stone from "A".
+     * @param jewels
+     * @param stones
+     * @return
+     */
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        for(int i = 0; i<jewels.length(); i++){
+            for(int j = 0; j< stones.length(); j++){
+                if(jewels.charAt(i) == stones.charAt(j)){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
