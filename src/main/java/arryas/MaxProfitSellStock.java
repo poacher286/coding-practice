a1d9d235e51c64d2b483773ad2cdee994ba0249a
poacher286 <bulandsahu50@gmail.com>
2022-08-06 21:43:44 +0530

    MaxProfitSellStock added

---
+++ src/main/java/arryas/MaxProfitSellStock.java
@@ -0,0 +1,13 @@
+package arryas;
+
+public class MaxProfitSellStock {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int profit = 0;
+        for(int price : prices) {
+            if(price < minPrice) minPrice = price;
+            if(price - minPrice > profit) profit = price - minPrice;
+        }
+        return profit;
+    }
+}
