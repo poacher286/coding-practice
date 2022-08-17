68da176bc3ad48b3812385d11efba2f3d156a4dd
poacher286 <bulandsahu50@gmail.com>
2022-08-17 17:47:32 +0530

    SearchInsert added

---
+++ src/main/java/twoPointers/LastStoneWeight.java
@@ -0,0 +1,49 @@
+package twoPointers;
+
+import java.util.Arrays;
+
+public class LastStoneWeight {
+    /**
+     * You are given an array of integers stones where stones[i] is the weight of the ith stone.
+     *
+     * We are playing a game with the stones. On each turn, we choose the heaviest two stones and smash them together. Suppose the heaviest two stones have weights x and y with x <= y. The result of this smash is:
+     *
+     * If x == y, both stones are destroyed, and
+     * If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+     * At the end of the game, there is at most one stone left.
+     *
+     * Return the weight of the last remaining stone. If there are no stones left, return 0.
+     * @param stones
+     * @return
+     */
+    public int lastStoneWeight(int[] stones) {
+        Arrays.sort(stones);
+        int len = stones.length - 1;
+        for (int i = len; i > 0; i--) {
+            if (stones[i] == stones[i - 1]) {
+                stones[i - 1] = Integer.MAX_VALUE;
+                stones[i] = Integer.MAX_VALUE;
+                i--;
+            } else {
+                stones[i - 1] = Math.abs(stones[i] - stones[i - 1]);
+                stones[i] = Integer.MAX_VALUE;
+            }
+            Arrays.sort(stones);
+        }
+        return stones[0] == Integer.MAX_VALUE ? 0 : stones[0];
+    }
+
+    public int lastStoneWeightRecurssion(int[] stones) {
+        if (stones.length == 1) return stones[0];
+        Arrays.sort(stones);
+        int res = stones[stones.length - 1] - stones[stones.length - 2];
+        int[] newarr = new int[stones.length - 1];
+        int ind = 0;
+        for (int i = 0; i < stones.length - 2; i++) {
+            newarr[ind] = stones[i];
+            ind++;
+        }
+        newarr[ind] = res;
+        return lastStoneWeightRecurssion(newarr);
+    }
+}
---
+++ src/main/java/twoPointers/SearchInsert.java
@@ -0,0 +1,31 @@
+package twoPointers;
+
+public class SearchInsert {
+    /**
+     * Given a sorted array of distinct integers and a target value,
+     * return the index if the target is found.
+     * If not, return the index where it would be if it were inserted in order.
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right){
+            int mid = left + (right - left) /2;
+            if (nums[mid] == target) return mid;
+            else if(nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,3,5,6};
+        int target = 7;
+        System.out.println(searchInsert(nums, target));
+    }
+}
