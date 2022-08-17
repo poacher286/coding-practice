9cef9f795a14ecba1a37610b677e7943aecee6f2
poacher286 <bulandsahu50@gmail.com>
2022-08-17 22:41:32 +0530

    ReverseOnlyLetters added

---
+++ src/main/java/strings/ReverseOnlyLetters.java
@@ -0,0 +1,24 @@
+package strings;
+
+public class ReverseOnlyLetters {
+    public static String reverseOnlyLetters(String S) {
+        StringBuilder ans = new StringBuilder();
+        int j = S.length() - 1;
+        for (int i = 0; i < S.length(); ++i) {
+            if (Character.isLetter(S.charAt(i))) {
+                while (!Character.isLetter(S.charAt(j)))
+                    j--;
+                ans.append(S.charAt(j--));
+            } else {
+                ans.append(S.charAt(i));
+            }
+        }
+
+        return ans.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "a-bC-dEf-ghIj";
+        System.out.println(reverseOnlyLetters(s));
+    }
+}
