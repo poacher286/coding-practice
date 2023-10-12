4170456fbae5c1d1be1088a166a73e93fa9c5b90
poacher286 <bulandsahu50@gmail.com>
2023-10-12 22:25:52 +0530

    DeleteNode added

---
+++ src/main/java/linkedList/DeleteNode.java
@@ -0,0 +1,13 @@
+package linkedList;
+
+public class DeleteNode {
+    public void deleteNode(ListNode node) {
+        // Since we know input node is not last node, so nextNode will not be null
+        ListNode nextNode = node.next;
+        // Step 2
+        node.val = nextNode.val;
+        // Step 3
+        node.next = nextNode.next;
+        nextNode.next = null;
+    }
+}
