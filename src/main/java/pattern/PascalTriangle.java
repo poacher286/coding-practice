6debb8ceaebefb525e126e0f09d14c76dd721199
poacher286 <bulandsahu50@gmail.com>
2023-10-17 22:06:44 +0530

    PascalTriangle added

--- src/main/java/pattern/PascalTriangle.java
+++ src/main/java/pattern/PascalTriangle.java
@@ -1,7 +1,10 @@
 package pattern;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Input: numRows = 5
@@ -31,10 +34,45 @@ public class PascalTriangle {
         return ans;
     }
 
+    public static List<Integer> getRow(int rowIndex) {
+        List<BigInteger> pascalTriangle = new ArrayList<>();
+        int rowEle = rowIndex + 1;
+        for (int r = 0; r < rowEle; r++) {
+            //nCr = n!/r!(n-r)!
+            BigInteger nCr = factorialBigInt(BigInteger.valueOf(rowIndex))
+                            .divide(factorialBigInt(BigInteger.valueOf(r)).multiply(factorialBigInt(BigInteger.valueOf(rowIndex - r))));
+            pascalTriangle.add(nCr);
+        }
+        return pascalTriangle
+                .stream()
+                .mapToInt(BigInteger::intValue)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
+
+    public static long factorial(int x, Map<Integer, Long> factMap){
+        if (x <= 1){
+            return 1;
+        }
+        if (factMap.containsKey(x)) {
+            return factMap.get(x);
+        }
+        factMap.putIfAbsent(x, x * factorial(x - 1, factMap));
+        return factMap.get(x);
+    }
+
+    public static BigInteger factorialBigInt(BigInteger num) {
+        if (num.compareTo(BigInteger.TWO) < 0) return BigInteger.ONE;
+        return num.multiply(factorialBigInt(num.subtract(BigInteger.ONE)));
+    }
+
 
     public static void main(String[] args) {
         int n = 6;
+        int rowIndex = 21;
         System.out.println(generate(n));
+        System.out.println(getRow(rowIndex));
     }
 
 }
