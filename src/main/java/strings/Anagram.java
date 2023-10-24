9a1f274f1916869553df1e430846ae1b1f389249
poacher286 <bulandsahu50@gmail.com>
2023-10-24 19:53:38 +0530

    GroupAnagram added

--- src/main/java/strings/Anagram.java
+++ src/main/java/strings/Anagram.java
@@ -1,6 +1,8 @@
 package strings;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Anagram {
     public static boolean isAnagram(String[] str) {
@@ -21,6 +23,39 @@ public class Anagram {
             }
         }
         return count == str.length;
+    }
+
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> count = new HashMap<>();
+
+        // Count the frequency of characters in string s
+        for (char x : s.toCharArray()) {
+            count.put(x, count.getOrDefault(x, 0) + 1);
+        }
+
+        // Decrement the frequency of characters in string t
+        for (char x : t.toCharArray()) {
+            count.put(x, count.getOrDefault(x, 0) - 1);
+        }
+
+        // Check if any character has non-zero frequency
+        for (int val : count.values()) {
+            if (val != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isAnagram1(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+
+        return Arrays.equals(sChars, tChars);
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/strings/GroupAnagram.java
@@ -0,0 +1,36 @@
+package strings;
+
+import java.util.*;
+
+/**
+ * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+ *
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: strs = ["eat","tea","tan","ate","nat","bat"]
+ * Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ */
+public class GroupAnagram {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+            map.putIfAbsent(sortedWord, new ArrayList<>());
+            map.get(sortedWord).add(word);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+}
