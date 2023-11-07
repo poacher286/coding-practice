91c207841e73a532db66f7ce53d4797ac6487e6c
poacher286 <bulandsahu50@gmail.com>
2023-11-07 18:22:37 +0530

    SeatManager added

---
+++ src/main/java/priorityQueue/SeatManager.java
@@ -0,0 +1,29 @@
+package priorityQueue;
+
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
+
+class SeatManager {
+    private PriorityQueue<Integer> availableSeats;
+
+    public SeatManager(int n) {
+        // Initially all seats are unreserved.
+        availableSeats = new PriorityQueue<>();
+        for (int seatNumber = 1; seatNumber <= n; ++seatNumber) {
+            availableSeats.offer(seatNumber);
+        }
+    }
+    
+    public int reserve() {
+        // Get the smallest-numbered unreserved seat from the min heap.
+        int seatNumber = availableSeats.poll();
+        return seatNumber;
+    }
+    
+    public void unreserve(int seatNumber) {
+        // Push the unreserved seat back into the min heap.
+        availableSeats.offer(seatNumber);
+    }
+
+}
