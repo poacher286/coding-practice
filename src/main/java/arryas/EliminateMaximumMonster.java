a8d097dda28dbb12f5de449ccdeeb4ac913187c4
poacher286 <bulandsahu50@gmail.com>
2023-11-07 19:13:07 +0530

    EliminateMaximumMonster added

---
+++ src/main/java/arryas/EliminateMaximumMonster.java
@@ -0,0 +1,47 @@
+package arryas;
+
+
+import java.util.Arrays;
+
+/**
+ * You are playing a video game where you are defending your city from a group of n monsters. You are given a 0-indexed integer array dist of size n, where dist[i] is the initial distance in kilometers of the ith monster from the city.
+ *
+ * The monsters walk toward the city at a constant speed. The speed of each monster is given to you in an integer array speed of size n, where speed[i] is the speed of the ith monster in kilometers per minute.
+ *
+ * You have a weapon that, once fully charged, can eliminate a single monster. However, the weapon takes one minute to charge. The weapon is fully charged at the very start.
+ *
+ * You lose when any monster reaches your city. If a monster reaches the city at the exact moment the weapon is fully charged, it counts as a loss, and the game ends before you can use your weapon.
+ *
+ * Return the maximum number of monsters that you can eliminate before you lose, or n if you can eliminate all the monsters before they reach the city.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: dist = [1,3,4], speed = [1,1,1]
+ * Output: 3
+ */
+public class EliminateMaximumMonster {
+    public static int eliminateMaximum(int[] dist, int[] speed) {
+        int[] time = new int[dist.length];
+        for (int i = 0; i < time.length; i++) {
+            time[i] = (int) Math.ceil((double)dist[i]/speed[i]);
+        }
+        Arrays.sort(time);
+        int count = 1;
+        int timePassed = 1;
+        for (int i = 1; i < time.length; i++) {
+            if (time[i] - timePassed <= 0){
+                return count;
+            }
+            count++;
+            timePassed += 1;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] dist = {3,5,7,4,5}, speed = {2,3,6,3,2};
+        System.out.println(eliminateMaximum(dist, speed));
+    }
+}
