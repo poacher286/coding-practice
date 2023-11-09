7ebba36487f2214f2e0566cb66208f284e440082
poacher286 <bulandsahu50@gmail.com>
2023-11-09 22:24:57 +0530

    Ugly added

---
+++ src/main/java/numbers/Ugly.java
@@ -0,0 +1,36 @@
+package numbers;
+
+public class Ugly {
+    public boolean isUgly(int n) {
+        if (n < 1) return false;
+        while (n % 2 == 0) {
+            n = n / 2;
+        }
+        while (n % 3 == 0) {
+            n = n / 3;
+        }
+        while (n % 5 == 0) {
+            n = n / 5;
+        }
+
+        return n == 1;
+    }
+
+    public int nthUglyNumber(int n) {
+        int[] ugly = new int[n];
+        ugly[0] = 1;
+        int index2 = 0, index3 = 0, index5 = 0;
+        int factor2 = 2, factor3 = 3, factor5 = 5;
+        for(int i=1;i<n;i++){
+            int min = Math.min(Math.min(factor2,factor3),factor5);
+            ugly[i] = min;
+            if(factor2 == min)
+                factor2 = 2*ugly[++index2];
+            if(factor3 == min)
+                factor3 = 3*ugly[++index3];
+            if(factor5 == min)
+                factor5 = 5*ugly[++index5];
+        }
+        return ugly[n-1];
+    }
+}
