7c92b7e8948ab5d0ea986e9581a0c316fcb82df7
poacher286 <bulandsahu50@gmail.com>
2023-12-06 17:35:56 +0530

    LongestPalindromeSubstring updated

--- src/main/java/strings/LongestPalindromeSubstring.java
+++ src/main/java/strings/LongestPalindromeSubstring.java
@@ -40,8 +40,42 @@ public class LongestPalindromeSubstring {
         }
     }
 
+
+    private static String longestPalindrome2(String s){
+        if (s == null || s.length() < 1){
+            return "";
+        }
+
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = extendFromMid(s, i, i);//if odd length
+            int len2 = extendFromMid(s, i, i+1);//if even length
+            int len = Math.max(len1, len2);
+
+            if (len > end - start){
+                start = i - (len - 1)/ 2;
+                end = i + len / 2;
+            }
+        }
+
+        return s.substring(start, end + 1);
+    }
+
+    private static int extendFromMid(String s, int left, int right){
+        int l = left;
+        int r = right;
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)){
+            l--;
+            r++;
+        }
+        return r - l - 1;
+    }
+
     public static void main(String[] args) {
         String s = "babad";
         System.out.println(longestPalindrome(s));
+        System.out.println(longestPalindrome2(s));
     }
 }
