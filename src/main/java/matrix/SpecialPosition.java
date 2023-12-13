b888b9194923817bbc237aacc32a35b3694a9966
poacher286 <bulandsahu50@gmail.com>
2023-12-13 19:47:26 +0530

    SpecialPosition added

---
+++ src/main/java/matrix/SpecialPosition.java
@@ -0,0 +1,33 @@
+package matrix;
+
+public class SpecialPosition {
+    public int numSpecial(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        int[] rowCount = new int[m];
+        int[] colCount = new int[n];
+
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (mat[row][col] == 1){
+                    rowCount[row]++;
+                    colCount[col]++;
+                }
+            }
+        }
+
+        int result = 0;
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (mat[row][col] == 0){
+                    continue;
+                }
+                if (rowCount[row] == 1 && colCount[col] == 1){
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+}
