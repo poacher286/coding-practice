b6204a866a32b4f679709bbd99750d043370fa17
poacher286 <bulandsahu50@gmail.com>
2023-12-20 21:37:09 +0530

    FlattenBinaryTreeToLinkedList added

---
+++ src/main/java/tree/FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,19 @@
+package tree;
+
+public class FlattenBinaryTreeToLinkedList {
+    public void flatten(TreeNode root) {
+        TreeNode current = root;
+        while (current != null){
+            if (current.left != null){
+                TreeNode temp = current.left;
+                while (temp.right != null){
+                    temp = temp.right;
+                }
+                temp.right = current.right;
+                current.right = current.left;
+                current.left = null;
+            }
+            current = current.right;
+        }
+    }
+}
